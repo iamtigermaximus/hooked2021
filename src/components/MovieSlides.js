4dac28b7ddaa43235ec2aa17dfbe367a721b9658
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-10-07 08:41:50 +0300

    update react version

--- src/components/MovieSlides.js
+++ src/components/MovieSlides.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 
 const MovieSlides = () => {
   const [nowPlaying, setNowPlaying] = useState([])
